214ef73813e429c88e724f4fa53acd925fde1f92
svpalacio <svpalacio74@gmail.com>
2024-08-21 20:25:23 -0300

    PreEntrega1-PalacioSoledad

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,18 @@
+import {Flex,Text } from "@chakra-ui/react";
+import { FaShoppingCart } from "react-icons/fa";
+
+const CartWidget = () => {
+    return
+      <Flex
+         alignItems={"center"}
+         height={"100%"}
+         justifyContent={"space-between"}
+         width={"60px"}
+      >
+        <FaShoppingCart size={30}/>
+        <Text fontSize={"1.5rem"}>1</Text>
+      </Flex>
+} 
+
+
+export default CartWidget;
