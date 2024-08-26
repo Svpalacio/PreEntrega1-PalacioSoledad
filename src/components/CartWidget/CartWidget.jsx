7367289006353c6a964e1bb958ce077078d197a7
svpalacio <svpalacio74@gmail.com>
2024-08-25 21:16:45 -0300

    arreglando el CartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,17 +2,19 @@ import {Flex,Text } from "@chakra-ui/react";
 import { FaShoppingCart } from "react-icons/fa";
 
 const CartWidget = () => {
-    return
-      <Flex
-         alignItems={"center"}
-         height={"100%"}
-         justifyContent={"space-between"}
-         width={"60px"}
-      >
-        <FaShoppingCart size={30}/>
-        <Text fontSize={"1.5rem"}>1</Text>
-      </Flex>
-} 
+  return (
+    <Flex
+      alignItems={"center"}
+      height={"100%"}
+      justifyContent={"space-between"}
+      width={"60px"}
+    >
+      <FaShoppingCart size={30}/>
+      <Text fontSize={"1.5rem"}>0</Text>
+    </Flex>
+  );
+};
+
 
 
 export default CartWidget;
